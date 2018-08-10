99b732bbf8669247ea842e9af40cf204fbbae535
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-08-10 13:22:16 -0300

    tests empresa service

--- src/test/java/com/leonardo/pontointeligente/api/services/EmpresaServiceTest.java
+++ src/test/java/com/leonardo/pontointeligente/api/services/EmpresaServiceTest.java
@@ -1,5 +1,52 @@
 package com.leonardo.pontointeligente.api.services;
 
-public class EmpresaServiceTest {
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.leonardo.pontointeligente.api.entities.Empresa;
+import com.leonardo.pontointeligente.api.repositories.EmpresaRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class EmpresaServiceTest {
+	
+	@MockBean
+	private EmpresaRepository empresaRespository;
+	
+	@Autowired
+	private EmpresaService empresaService;
+	
+	private static final String CNPJ = "11122234567898";
+	
+	@Before
+	public void setUp() throws Exception {
+		BDDMockito.given(this.empresaRespository.findByCnpj(Mockito.anyString())).willReturn(new Empresa());
+		BDDMockito.given(this.empresaRespository.save(Mockito.any(Empresa.class))).willReturn(new Empresa());
+	}
+	
+	@Test
+	public void testBuscarEmpresaPorCnpj() {
+		Optional<Empresa> empresa = this.empresaService.buscarPorCnpj(CNPJ);
+		assertTrue(empresa.isPresent());
+	}
+	
+	@Test
+	public void testPersistirEmpresa() {
+		Empresa empresa = this.empresaService.persistir(new Empresa());
+		assertNotNull(empresa);
+	}
 }
