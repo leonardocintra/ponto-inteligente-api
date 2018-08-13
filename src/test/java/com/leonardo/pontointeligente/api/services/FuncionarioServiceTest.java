61f4f4ab2147aef510519c2ab34642f92bd7f98c
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-08-13 11:57:42 -0300

    adiconado servico de funcionario

--- src/test/java/com/leonardo/pontointeligente/api/services/FuncionarioServiceTest.java
+++ src/test/java/com/leonardo/pontointeligente/api/services/FuncionarioServiceTest.java
@@ -1,6 +1,12 @@
 package com.leonardo.pontointeligente.api.services;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Optional;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
@@ -27,9 +33,33 @@ public class FuncionarioServiceTest {
 	@Before
 	public void setUp() throws Exception {
 		BDDMockito.given(this.funcionarioRepository.save(Mockito.any(Funcionario.class))).willReturn(new Funcionario());
-		//BDDMockito.given(this.funcionarioRepository.findById(Mockito.anyLong())).willReturn(new Funcionario());		
+		BDDMockito.given(this.funcionarioRepository.findById(Mockito.anyLong())).willReturn(Optional.ofNullable(new Funcionario()));
 		BDDMockito.given(this.funcionarioRepository.findByEmail(Mockito.anyString())).willReturn(new Funcionario());
 		BDDMockito.given(this.funcionarioRepository.findByCpf(Mockito.anyString())).willReturn(new Funcionario());
+	}
+	
+	@Test
+	public void testPersistirFuncionario() {
+		Funcionario funcionario = this.funcionarioService.persistir(new Funcionario());
+		assertNotNull(funcionario);
+	}
+	
+	@Test
+	public void testBuscarFuncionarioPorId() {
+		Optional<Funcionario> funcionario = this.funcionarioService.buscarPorId(1L);
+		assertTrue(funcionario.isPresent());
+	}
+	
+	@Test
+	public void testBuscarFuncionarioPorEmail() {
+		Optional<Funcionario> funcionario = this.funcionarioService.bucarPorEmail("email@teste.com.br");
+		assertTrue(funcionario.isPresent());
+	}
+	
+	@Test
+	public void testBuscarFuncionarioPorCPF() {
+		Optional<Funcionario> funcionario = this.funcionarioService.buscarPorCpf("12345678901");
+		assertTrue(funcionario.isPresent());
 	}
 
 }
